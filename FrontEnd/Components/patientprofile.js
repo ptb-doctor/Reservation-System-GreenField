38b8b8f245a418c75857b1d698bb526821984eb8
kamellababidi <kamelallababidi@gmail.com>
2017-10-09 19:14:40 +0300

    fix patient profile

--- FrontEnd/Components/patientprofile.js
+++ FrontEnd/Components/patientprofile.js
@@ -1,18 +1,18 @@
-angular.module('app')
-.component('patientprofile', {
-        controller: "patientprofileCtrl",
-        templateUrl: `./views/patientprofile.html`
- })
-.controller('patientprofileCtrl', function($scope, $http) {
-    console.log('kaaaamel')
-	$scope.profile;
-	$.ajax({
+window.onload = function() {
+console.log('hii')
+var profile;
+$.ajax({
                 url: '/patientprofile',
                 dataType: 'json',
                 async: false,
                 success: function(data) {
                     console.log(data)
-                   $scope.profile=data 
+                    document.getElementById("image").src=data[0].image;
+                    document.getElementById("name").textContent=data[0].name;
+                    document.getElementById("phone").textContent=data[0].phone;
+
                 }
-            });
-})
+        });
+}
+       //when the document is finished loading, replace everything
+       //between the <a ...> </a> tags with the value of splitText
